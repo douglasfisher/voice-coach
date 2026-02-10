50f2ad2da3865a23bbb027e24a57a3060dce54a2
douglasfisher <doug@douglasfisher.co.uk>
2026-02-10 22:10:10 +0000

    🏁 Session end: 20260210_221010 (       2 files)

--- components/ui/HeaderFade.tsx
+++ components/ui/HeaderFade.tsx
@@ -4,7 +4,7 @@ export function HeaderFade() {
   return (
     <LinearGradient
       colors={['rgba(10,10,15,0.9)', 'transparent']}
-      style={{ height: 50 }}
+      style={{ height: 80 }}
       pointerEvents="none"
     />
   );
